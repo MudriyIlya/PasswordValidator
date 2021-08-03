8692652e996d9acdfa8e5938cc230275c65246ce
Илья Мудрый <mudriy.ilya@gmail.com>
2021-08-03 16:41:59 +0300

    Added Password Validation Framework

---
+++ PasswordValidator/PasswordValidator.h
@@ -0,0 +1,18 @@
+//
+//  PasswordValidator.h
+//  PasswordValidator
+//
+//  Created by Илья Мудрый on 03.08.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PasswordValidator.
+FOUNDATION_EXPORT double PasswordValidatorVersionNumber;
+
+//! Project version string for PasswordValidator.
+FOUNDATION_EXPORT const unsigned char PasswordValidatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PasswordValidator/PublicHeader.h>
+
+
---
+++ PasswordValidator/PasswordValidator.swift
@@ -0,0 +1,34 @@
+// Supported frameworks
+import Foundation
+
+/**
+ 
+ Validate Password
+ 
+ ### Check your password
+ 
+ - Author:
+ ILYA MUDRIY
+ 
+ - returns:
+ `Bool if the password is correct
+ 
+*/
+class PasswordValidator {
+    
+    /**
+     - Check your password with this method
+     
+     - parameters:
+        - password: String with password. Cannot be empty.
+     
+     - returns:
+     `true` if the password is correct or `false`
+     */
+    public static func validate(_ password: String) -> Bool {
+        
+        let passwordTest = NSPredicate(format: "SELF MATCHES %@", "^(?=.*[A-Z].*[A-Z])(?=.*[!@#$&*])(?=.*[0-9].*[0-9])(?=.*[a-z].*[a-z].*[a-z]).{8,16}$")
+        return passwordTest.evaluate(with: password)
+    
+    }
+}
